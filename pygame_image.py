d098f46d8a5e7ca4c19b2cb3a846c10cc7d1a1ff
fushimity@edu.teu.ac.jp <c0a22145bf@edu.teu.ac.jp>
2023-11-21 17:37:57 +0900

    一回目

---
+++ pygame_image.py
@@ -0,0 +1,33 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kt_img = pg.image.load("ex01/fig/3.png")
+    #kt_img = pg.transform.flip(kt_img, True, False)
+    #kt_imgs = [kt_img, pg.transform.rotozoom(kt_img, 10, 1.0)]
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        x = tmr%2800
+        screen.blit(bg_img, [0, 0])
+        screen.blit(pg.transform.flip(bg_img, True, False), [1600-x, 0])
+        #screen.blit(bg_img, [800-x, 0])
+        #screen.blit(kt_imgs[tmr%2], (300, 200))
+        screen.blit(kt_img, (300, 200))
+        pg.display.update()
+        tmr += 1      
+        clock.tick(500)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
